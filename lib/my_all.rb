ef5135bd6f8a2f9cb739bafc40ba28f3789943b0
Jeremy Bourne <jbourne25@yahoo.com>
2020-01-27 18:17:49 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,5 +3,8 @@ require 'pry'
 def my_all?(collection)
   i = 0 
   new_to_collection = [] 
-if collection.length < 
+while i < collection.length 
+  new_to_collection << yield(collection[i])
+  i += 1 
+  end 
 end
